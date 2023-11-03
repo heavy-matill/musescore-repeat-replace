c8a509b8f75eb41a199753b03e4c0727e93a0e16
heavy-matill <heavymatill@gmail.com>
2023-11-03 17:37:46 +0100

    replace voice only

--- run.js
+++ run.js
@@ -33,18 +33,23 @@ let jObj = parser.parse(content);
 console.log(Object.keys(jObj.museScore.Score.Staff))
 for (let k in jObj.museScore.Score.Staff) {
     let i = 0;
+    // run through all measures
     while (i < jObj.museScore.Score.Staff[k].Measure.length) {
         let rep = jObj.museScore.Score.Staff[k].Measure[i].measureRepeatCount ?? 0
+        // if a measure contains measureRepeatCount = 1
         if (rep == 1) {
+            // find following measureRepeatCounts e.g. 1, 1-2 or 1-4
             while ((jObj.museScore.Score.Staff[k].Measure[i + rep]?.measureRepeatCount ?? 0) > rep) {
                 rep++
             }
             let j = i - rep
+            // replace voice of the repeating measures by the reference (e.g. measure before, or 1-2 measures before...)
             while (j < i) {
-                jObj.museScore.Score.Staff[k].Measure[j + rep] = jObj.museScore.Score.Staff[k].Measure[j]
+                jObj.museScore.Score.Staff[k].Measure[j + rep].voice = jObj.museScore.Score.Staff[k].Measure[j].voice
                 j++
             }
         }
+        // skip over the current measure or all the forthfollwing repeated measures (e.g. 1-2, 1-4)
         i += Math.max(1, rep)
     }
 }
